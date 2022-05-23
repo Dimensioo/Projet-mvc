139c510e0f372c385525057bd034a92d3bcdfc05
Maxime Walther <87872822+Dimensioo@users.noreply.github.com>
2022-05-23 16:58:44 +0200

    23/05/2022

--- views/template.php
+++ views/template.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <?= $style ?>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="icon" type="image/png" href="<?=URL?>images/favicon.png"/>
     <script src="https://kit.fontawesome.com/3df32f415a.js" crossorigin="anonymous"></script>
     <script src="<?=URL?>js/displayRanking.js" defer></script>
@@ -18,11 +18,11 @@
             <div id="nav1">
                 <div><h1>Game List <i class="fas fa-gamepad"></i></h1></div>
                 <?php
-                    if(isset($_SESSION["pseudo"])) { //afichage quand utilisateur connécté
-                        echo "<div><p>Bienvenue ", $_SESSION['pseudo'], "</p></div>";
+                    if(isset($_SESSION["pseudo"])) { //affichage quand utilisateur connécté
+                        echo "<div><p>Bienvenue ".$_SESSION['pseudo']."</p></div>";
                         echo "<div class=\"sizeup\"><a href=\"".URL."logout\">Déconnexion</a></div>";
                     }
-                    else { //afichage quand utilisateur déconecté
+                    else { //affichage quand utilisateur déconecté
                         echo "<div class=\"sizeup\"><a href=\"".URL."connexion\">Se connecter / S'inscrire</a></div>";
                     }
                 ?>
@@ -32,7 +32,7 @@
                     <?php if(empty($url)){$url[0] = 'accueil';} ?>
                     <div><a href="<?=URL?>accueil" <?php if($url[0] && $url[0] == 'accueil'){echo 'id="active"';}?>><i class="fas fa-home"></i></a></div>
                     <div><a href="<?=URL?>jeux" <?php if($url[0] == 'jeux' || $url[0] == 'recherche' || $url[0] == 'classement'){echo 'id="active"';}?>>Jeux</a></div>
-                    <?php 
+                    <?php
                         if($_SESSION && $_SESSION["pseudo"] && $url[0] != 'liste'){echo "<div><a href=\"".URL."liste/".$_SESSION["id"]."\">Liste</a></div>";}
                         else if($_SESSION && $_SESSION['pseudo'] && $url[0] == 'liste'){echo "<div><a href=\"".URL."liste/".$_SESSION["id"]."\" id=\"active\">Liste</a></div>";}
                         else{echo "<div><a href=\"".URL."liste\">Liste</a></div>";}
