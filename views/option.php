f8730923d1445ea29c45907e4f1ab7cfcbf5bcef
Maxime Walther <87872822+Dimensioo@users.noreply.github.com>
2022-06-16 16:33:22 +0200

    Update option.php

--- views/option.php
+++ views/option.php
@@ -5,26 +5,19 @@
     <h2>Informations du compte</h2> <!--Resumé information du compte-->
     <div>
         <div>
-            <p><b>Pseudo : </b><?php if($_SESSION){echo $_SESSION['pseudo'];}?></p>
-            <p><b>Adresse E-mail : </b><?php if($_SESSION){echo $_SESSION['email'];}?></p>
-            <p><b>Inscrit depuis le : </b><?php if($_SESSION){echo $_SESSION['date'];}?></p>
+            <p><b>Pseudo : </b><?= $_SESSION['pseudo'] ?></p>
+            <p><b>Adresse E-mail : </b><?= $_SESSION['email'] ?></p>
+            <p><b>Inscrit depuis le : </b><?= $_SESSION['date'] ?></p>
         </div>
         <div>
-            <?php //Affichage image de profil utilisateur
-                if($_SESSION){
-                    echo '<img src="'.$_SESSION['image'].'" alt="Image de profil" class="sizeup" height=150px>';
-                }
-                else{
-                    echo '<img src="images/img_users/default_user.png" alt="Image de profil" class="sizeup" height=150px>';
-                }
-            ?>
+            <img src="<?= $_SESSION['image'] ?>" alt="Image de profil" class="sizeup" height=150px>
         </div>
     </div>
     <h2>Modifier Informations <i class="fas fa-cog"></i></h2> <!--Bouton d'option pour modifier informations du compte-->
     <div>
         <a onClick="change_pseudo()" href="#" class="sizeup">Modifier pseudo</a>
         <a onClick="change_pic()" href="#" class="sizeup">Modifier image de profil</a>
-        <a onClick="change_mail()" href="#" class="sizeup">Modifier adrese E-mail</a>
+        <a onClick="change_mail()" href="#" class="sizeup">Modifier adresse E-mail</a>
         <a onClick="change_mdp()" href="#" class="sizeup">Modifier mot de passe</a>
         <a onClick="delete_account()" href="#" class="sizeup">Suprimer mon compte <i class="far fa-trash-alt"></i></a>
     </div>            
@@ -58,7 +51,7 @@
         <input type="submit" value="Modifier mot de passe" class="sizeup">
         <?php include('controllers/update/updatePassword.php') ?>
     </form>
-    <form action="" method="post" class="invisible" id="delete_account"> <!--formulaire supression du compte -->
+    <form action="" method="post" class="invisible" id="delete_account"> <!--formulaire suppression du compte -->
         <label for="verif_del_mdp">Entrer votre mot de passe</label><br>
         <input type="password" name="verif_del_mdp" required><br>
         <input type="submit" value="Suprimer mon compte" class="sizeup">
